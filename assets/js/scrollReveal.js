9381c671e813d1bc5997e17a38b2012edb058c23
frilii <gualbertofabricio4@gmail.com>
2024-04-15 20:05:38 -0300

    portfolio

--- assets/js/scrollReveal.js
+++ assets/js/scrollReveal.js
@@ -8,6 +8,6 @@ export function initScrollReveal() {
   sr.reveal(".delaySmallReveal", { delay: 200 });
   sr.reveal(".delayMediumReveal", { delay: 300 });
   sr.reveal(".delayLargeReveal", { delay: 400 });
-
+  sr.reveal(".delayExtraBigReveal", { delay: 600 });
   sr.reveal(".intervalCardReveal", { interval: 400 });
 }
